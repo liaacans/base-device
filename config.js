cadc6a9a9c55bf0cdb73e8b0a9876e54c4082107
Aulia Rahman <114418374+liaacans@users.noreply.github.com>
2022-10-06 20:45:38 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,59 @@
+/* 
+NOTE : JGN HPS DIBAWAH INI!!
+SC ASLI : ZEROYT7
+RECODE : RAHMXBOT X RAHMAN GNTENG:V
+WHATSAPP : 085821676621
+NOMOR BOT : 085821369324
+NOTE : JGN HPS AUTHORNYA!!!
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//━━━━━━━━━━━━━━━[ WEBSITE APIKEY ]━━━━━━━━━━━━━━━━━//
+
+global.APIs = {
+liaacans: 'https://zenzapis.xyz',
+}
+
+//━━━━━━━━━━━━━━━[ APIKEY ]━━━━━━━━━━━━━━━━━//
+
+global.APIKeys = {
+'https://zenzapis.xyz': '0e92565522',
+}
+
+//━━━━━━━━━━━━━━━[ SETTING ]━━━━━━━━━━━━━━━━━//
+global.thumb = fs.readFileSync('./image/thumbnail.jpg') // JGN DI UBAH!
+global.name = 'Aulia Rahman (Owner)', // GNTI NAMA LU!
+global.creator = 'LiaaCans BOT' // GANTI NAMA BOT LU BANH!
+global.owner = ['6285821676621'] // GNTI NOMOR LU!
+global.premium = ['6285821676621']
+global.pulsa = ['081528965381'] // UBAH NOMOR YG AKTIF!
+global.botname = 'LiaaCans BOT' // GNTI NAMA BOT LU!
+global.packname = 'Nih Stikel Umtukmu' // TRSERAH MO GANTI AP!
+global.author = 'LiaaCans BOT' // GNTI NMA LU!
+global.prefa = ['','!','.','/','#','$']
+global.sp = '⭔' // JGN DI UBAH²! NTR EROR
+global.mess = {
+success: '*[SUCCESS]* SUKSES PERMINTAAN ANDA TERKIRIM!',
+admin: 'Fitur Khusus Admin Group!',
+botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+owner: 'Fitur Khusus Owner Bot',
+group: 'Fitur Digunakan Hanya Untuk Group!',
+private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+bot: 'Fitur Khusus Pengguna Nomor Bot',
+wait: '*[WAIT]* SEDANG DIPROSES, HARAP TUNGGU SEBENTAR!',
+endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+premium: "Infinity",
+free: 100
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
