1d2c5a2d29d284ef08a4673a9fcf00cedeba8421
Aulia Rahman <114418374+liaacans@users.noreply.github.com>
2022-10-07 22:06:23 +0700

    Add files via upload

--- options/config.js
+++ options/config.js
@@ -1,6 +1,5 @@
 /* 
 NOTE : JGN HPS DIBAWAH INI!!
-SC ASLI : ZEROYT7
 RECODE : RAHMXBOT X RAHMAN GNTENG:V
 WHATSAPP : 085821676621
 NOMOR BOT : 085821369324
@@ -28,6 +27,7 @@ global.name = 'Aulia Rahman (Owner)', // GNTI NAMA LU!
 global.creator = 'LiaaCans BOT' // GANTI NAMA BOT LU BANH!
 global.owner = ['6285821676621'] // GNTI NOMOR LU!
 global.premium = ['6285821676621']
+global.quotes = 'Kesempatan Anda Untuk Sukses Di Setiap Kondisi Selalu Dapat Diukur Oleh Seberapa Besar Kepercayaan Anda Pada Diri Sendiri' // Ganti Quotes, Tapi Trsrh Klian
 global.pulsa = ['081528965381'] // UBAH NOMOR YG AKTIF!
 global.botname = 'LiaaCans BOT' // GNTI NAMA BOT LU!
 global.packname = 'Nih Stikel Umtukmu' // TRSERAH MO GANTI AP!
@@ -36,18 +36,19 @@ global.prefa = ['','!','.','/','#','$']
 global.sp = '⭔' // JGN DI UBAH²! NTR EROR
 global.mess = {
 success: '*[SUCCESS]* SUKSES PERMINTAAN ANDA TERKIRIM!',
-admin: 'Fitur Khusus Admin Group!',
+admin: 'Fitur Ini Khusus Untuk Admin Group!',
 botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
-owner: 'Fitur Khusus Owner Bot',
-group: 'Fitur Digunakan Hanya Untuk Group!',
-private: 'Fitur Digunakan Hanya Untuk Private Chat!',
-bot: 'Fitur Khusus Pengguna Nomor Bot',
+prem: 'Fitur Ini Khusus Untuk Premium',
+owner: 'Fitur Ini Khusus Untuk Owner Bot',
+group: 'Fitur Ini Digunakan Hanya Untuk Group!',
+private: 'Fitur Ini Digunakan Hanya Untuk Private Chat!',
+bot: 'Fitur Ini Khusus Pengguna Nomor Bot',
 wait: '*[WAIT]* SEDANG DIPROSES, HARAP TUNGGU SEBENTAR!',
 endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
 }
 global.limitawal = {
-premium: "Infinity",
-free: 100
+premium: "Unlimited",
+free: 10
 }
 
 let file = require.resolve(__filename)
