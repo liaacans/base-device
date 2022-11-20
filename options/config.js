d31020dcbf5f6708cf03b6fd4c3c437c08e4532f
Aulia Rahman <114418374+liaacans@users.noreply.github.com>
2022-11-20 20:48:11 +0700

    Add files via upload

--- options/config.js
+++ options/config.js
@@ -34,13 +34,27 @@ global.thumb = fs.readFileSync('./image/thumbnail.jpg') // JGN DI UBAH!
 global.name = 'Aulia Rahman (Owner)', // GNTI NAMA LU!
 global.creator = 'LiaaCans BOT' // GANTI NAMA BOT LU BANH!
 global.owner = ['6285821676621'] // GNTI NOMOR LU!
-global.premium = ['6285821676621']
+global.premium = ['6285821676621', '6281528965381']
+ // ADD PREM YG LU MAU BANH!!
 global.quotes = 'Kesempatan Anda Untuk Sukses Di Setiap Kondisi Selalu Dapat Diukur Oleh Seberapa Besar Kepercayaan Anda Pada Diri Sendiri' // Ganti Quotes, Tapi Trsrh Klian
 global.pulsa = ['081528965381'] // UBAH NOMOR YG AKTIF!
 global.botname = 'Created LiaaCans BOT' // GNTI NAMA BOT LU!
 global.packname = 'Nih Stikel Umtukmu' // TRSERAH MO GANTI AP!
 global.author = 'LiaaCans BOT' // GNTI NMA LU!
 global.fake = 'LIAACANS BOT VERIFIED' // GNTI NMA BOT LU BANH!
+// FITUR TOPUP OTOMATIS 
+global.hargadmff = 1261
+global.hargadmml = 1700
+global.merchant = 'M221101PAZJ5903YO' 
+global.secret = 'fcc9c3922337cd1443c071dbd3d356daebe4f908632e293241c31e8ac014c46f'
+global.signature = 'ad488c46183fa32611f547ffbc5e4e10'
+//CARA AKTIFIN:
+//1. DAFTAR DI APIGAMES.UD
+//2. AMBIL MERCHANT ID DAN SECRET KEY
+//3. DAFTAR DI UNIPIN
+//4. HUBUNGKAN AKUN UNIPIN DENGAN APIGANES DI BAGIAN KONEKSI
+//5. BUAT SIGNATURE DENGAN TOOLS ENCRYPT MD5 MERCHANTID+SECRETKEY
+//6 PASANG DIATAS SITU DAN SELESAI.
 global.prefa = ['','!','.','/','#','$']
 global.sp = '⭔' // JGN DI UBAH²! NTR EROR
 global.mess = {
