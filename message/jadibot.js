9808bccd86d7da96732a0df1cf99ccdba4d0b72c
Aulia Rahman <114418374+liaacans@users.noreply.github.com>
2022-11-20 20:46:47 +0700

    Add files via upload

--- message/jadibot.js
+++ message/jadibot.js
@@ -1,9 +1,18 @@
 // Fix Aja Kalau Bisa
-var { default: makeWaSocket, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, generateWAMessage, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require('@adiwajshing/baileys')
-var logg = (pino = require("pino"));
-var qrcode = require('qrcode');
-var { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('../message/myfunc')
-var store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+const { default: makeWaSocket, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, generateWAMessage, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require('@adiwajshing/baileys')
+const logg = (pino = require("pino"));
+const { Boom } = require('@hapi/boom');
+const yargs = require('yargs/yargs')
+const fs = require('fs')
+const FileType = require('file-type')
+const chalk = require('chalk')
+const path = require('path')
+const qrcode = require('qrcode');
+const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('../message/myfunc')
+const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+
+const storetes = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'storetes' }) })
+
 
 if (global.listJadibot instanceof Array) console.log()
 else global.listJadibot = []
@@ -11,34 +20,34 @@ else global.listJadibot = []
 const jadibot = async (liaacans, msg, from) => {
 const { sendImage, sendMessage } = liaacans;
 const { reply, sender } = m;
-const { state, saveState } = useSingleFileAuthState(`./session/jadibot/${num.split("@")[0]}`)
+let userbot = `${liaacans.decodeJid(liaacans.user.id)}`
+const { state, saveState } = useSingleFileAuthState(`./session/jadibot/@${userbot.split("@")[0]}`)
 try {
 async function startliaacans() {
 let { version, isLatest } = await fetchLatestBaileysVersion();
 const liaacans = await makeWaSocket({
 auth: state,
 printQRInTerminal: true,
-browser: ['Bot Numpang', "Chrome", "1.0.0"],
+browser: ['Bot Numpang', "Safari", "1.0.0"],
 logger: logg({ level: "silent" }),
 version,
 })
 
 liaacans.ev.on('messages.upsert', async chatUpdate => {
 try {
-kay = chatUpdate.messages[0]
-if (!kay.message) return
-kay.message = (Object.keys(kay.message)[0] === 'ephemeralMessage') ? kay.message.ephemeralMessage.message : kay.message
-if (kay.key && kay.key.remoteJid === 'status@broadcast') return
-if (!liaacans.public && !kay.key.fromMe && chatUpdate.type === 'notify') return
-if (kay.key.id.startsWith('BAE5') && kay.key.id.length === 16) return
+mek = chatUpdate.messages[0]
+if (!mek.message) return
+mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+if (!liaacans.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
+if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
 m = smsg(liaacans, m, store)
-require('../command/liaacans')(liaacans, m, chatUpdate, store)
+require("./command/liaacans")(liaacans, m, chatUpdate, store)
 } catch (err) {
 console.log(err)}
 })
 
 store.bind(liaacans.ev);
-liaacans.ev.on("creds.update", saveCreds);
 liaacans.ev.on("connection.update", async up => {
 const { lastDisconnect, connection } = up;
 if (connection == "connecting") return
@@ -54,7 +63,7 @@ global.listJadibot.push(liaacans)
 await m.reply(`*Connected to Whatsapp - Bot*\n\n*User :*\n _*× ID : ${liaacans.decodeJid(liaacans.user.id)}*_`)
 let user = `${liaacans.decodeJid(liaacans.user.id)}`
 let txt = `*Terdeteksi menumpang Jadibot*\n\n _× User : @${user.split("@")[0]}_`
-liaacans.sendMessage('6283834558105@s.whatsapp.net', {text: txt, mentions : [user]})
+liaacans.sendMessage('6285821676621@s.whatsapp.net', {text: txt, mentions : [user]})
 }
 
 if (connection === 'close') {
@@ -62,20 +71,22 @@ let reason = new Boom(lastDisconnect?.error)?.output.statusCode
 if (reason === DisconnectReason.badSession) { 
 console.log(`Bad Session File, Please Delete Session and Scan Again`); liaacans.logout(); }
 else if (reason === DisconnectReason.connectionClosed) { 
-console.log("Connection closed, reconnecting...."); start(); }
+console.log("Connection closed, reconnecting...."); startliaacans(); }
 else if (reason === DisconnectReason.connectionLost) { 
-console.log("Connection Lost from Server, reconnecting..."); start(); }
+console.log("Connection Lost from Server, reconnecting..."); startliaacans(); }
 else if (reason === DisconnectReason.connectionReplaced) { 
 console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); conn.logout(); }
 else if (reason === DisconnectReason.loggedOut) { 
-console.log(`Device Logged Out, Please Scan Again And Run.`); conn.logout(); }
+console.log(`Device Logged Out, Please Scan Again And Run.`); liaacans.logout(); }
 else if (reason === DisconnectReason.restartRequired) { 
-console.log("Restart Required, Restarting..."); start(); }
+console.log("Restart Required, Restarting..."); startliaacans(); }
 else if (reason === DisconnectReason.timedOut) { 
-console.log("Connection TimedOut, Reconnecting..."); start(); }
+console.log("Connection TimedOut, Reconnecting..."); startliaacans(); }
 else liaacans.end(`Unknown DisconnectReason: ${reason}|${connection}`)
 }
 })
+
+liaacans.ev.on('creds.update', saveState)
 
 liaacans.decodeJid = (jid) => {
 if (!jid) return jid
@@ -88,10 +99,20 @@ return decode.user && decode.server && decode.user + '@' + decode.server || jid
 liaacans.sendText = (jid, text, quoted = '', options) => liaacans.sendMessage(jid, { text: text, ...options }, { quoted })
 
 }
-start()
+startliaacans()
 } catch (e) {
 console.log(e)
 }
 }
 
+
 module.exports = { jadibot, listJadibot }
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update ${__filename}`))
+delete require.cache[file]
+require(file)
+})
